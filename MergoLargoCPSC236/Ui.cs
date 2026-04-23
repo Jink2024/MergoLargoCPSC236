008f07c075369bcb4076797a0b867577d8468fe9
Jink2024 <Pausch@chapman.edu>
2026-04-23 15:31:01 -0700

    I did nothing

--- MergoLargoCPSC236/Ui.cs
+++ MergoLargoCPSC236/Ui.cs
@@ -1,5 +1,8 @@
 ﻿public class Ui
 {
+    // sup people reading my comment
+    // Heres a riddle:
+    // What has a bed but never sleeps?
     public float ScoreMultiplier = 69f;
 
     public void DisplayScore(int score)
